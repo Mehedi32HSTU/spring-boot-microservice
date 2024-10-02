b143d2e583f0035adf94c569cb511be53ed1ce74
Md. Mehedi Hasan <mehedi32.cse.hstu@gmail.com>
2024-10-03 01:24:49 +0600

    Enable Hystrix in API Gateway Service

--- api-gateway/api-gateway/src/main/java/com/javabean/api_gateway/ApiGatewayApplication.java
+++ api-gateway/api-gateway/src/main/java/com/javabean/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,11 @@ package com.javabean.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
